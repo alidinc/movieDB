07a84f2235896d6183f07e26313ad99186691738
Ali Dinç <ali.dinc@wundermanthompson.com>
2022-11-21 16:32:41 +0000

    Initial commit

---
+++ MovieDbClip/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MovieDbClip
+//
+//  Created by Ali Dinç on 21/11/2022.
+//  Copyright © 2022 Alfian Losari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieDbClip/MovieDbClipApp.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDbClipApp.swift
+//  MovieDbClip
+//
+//  Created by Ali Dinç on 21/11/2022.
+//  Copyright © 2022 Alfian Losari. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MovieDbClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
